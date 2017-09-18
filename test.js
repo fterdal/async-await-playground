33c2b77165214e23593995d8ddb87afc4e61dbb8
Finn Terdal <fterdal@gmail.com>
2017-09-18 17:51:41 -0500

    Simple async await test

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
 const axios = require('axios');
+
+async function hiGoogle(name) {
+  const greeting = 'Hello ' + name;
+  const response = await axios.get('http://google.com');
+  console.log(response);
+  console.log(greeting);
+  return greeting + response;
+}
+
+// console.log(hiGoogle('finn'));
+hiGoogle('Finn')
