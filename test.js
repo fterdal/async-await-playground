efc8f4f5f8f651934581c239b100c0bcc8a37524
Finn Terdal <fterdal@gmail.com>
2017-09-18 18:04:40 -0500

    uses a more friendly url with placeholder data

--- test.js
+++ test.js
@@ -1,12 +1,15 @@
 const axios = require('axios');
 
-async function hiGoogle() {
-  const response = await axios.get('http://google.com');
-  return response;
+const rootURL = 'https://jsonplaceholder.typicode.com';
+async function fakePost() {
+  const response = await axios.get(rootURL + '/posts/1');
+  return response.data;
 }
 
 async function consLog(asFun) {
   const result = await asFun();
   console.log(result);
 }
-consLog(hiGoogle);
+consLog(fakePost);
+
+// console.log(fakePost); // This just returns an AsyncFunction object, not the result
